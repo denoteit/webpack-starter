415bf593af73600b0445dff8cfe28c78a6a2ad10
denoteit <ben@denote.it>
2019-11-18 19:50:49 +0000

    Added scss loader.

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
